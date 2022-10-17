8da555afbc9d0434181edc8f5349cc13977dea63
meliherz <ahmetmelih59@gmail.com>
2022-10-17 16:09:59 +0300

    Crud Unique

--- src/App.js
+++ src/App.js
@@ -1,47 +1,46 @@
-import React, { useState, Component } from 'react';
-import axios from 'axios';
+import axios from 'axios'
+import React, { Component } from 'react'
+import FlatList from 'flatlist-react'
 
 export default class App extends Component {
   constructor(){
     super()
     this.state={
-      kullanıcılar:[],
-      kullanıcı_ekranı:true,
-      kullanıcı_ekleme_ekranı:false
+      users:""
     }
   }
-  componentDidMount(){
-    axios
-    .get("https://jsonplaceholder.typicode.com/users")
-    .then(res=>{this.setState({
-      kullanıcılar:res.data
-    })})
+  async componentDidMount(){
+await axios.get("https://jsonplaceholder.typicode.com/users").
+then(res=>{this.setState({
+  users:res.data
+})
+console.log(this.state.users)})
   }
   render() {
-    const {kullanıcılar,kullanıcı_ekranı,kullanıcı_ekleme_ekranı} =this.state;
     return (
       <div className='container'>
-        {kullanıcı_ekleme_ekranı&&
-        <form>
-          <input placeholder='Email'/>
-          <input placeholder='Kullanıcı Adı'/>
-        </form>
-        }
-      {kullanıcı_ekranı&&
         <div className='row'>
-        <div className='col-12-l bg-info d-flex flex-column align-items-center pb-2'><h1>Users</h1>
-        <button className='btn btn-success'>Kullanıcı Ekle</button></div>
-      <div className='row'>
-        {
-          kullanıcılar.map(kullanıcı=>{
-            return(
-              <div className='col-12-l bg-ligth'>{kullanıcı.name}</div>
-            )
-          })
-        }
+<div className='col-12-l bg-success d-flex justify-content-center'><h1>USERS</h1></div>
+        <div className='col-12-l bg-light'>
+        <ul className='list-group'>
+        <FlatList
+          list={this.state.users}
+          renderItem={this.renderUser}
+          renderWhenEmpty={() => <div>List is empty!</div>}
+        />
+        </ul>
+        </div>
+        </div>
       </div>
-      </div>}
-         </div>
     )
+  }
+  renderUser=(user,id)=>{
+return(
+<li key={id} className="list-group-item d-flex">
+  <p className='mx-2'>{user.id}</p>
+  <p className='mx-5'>{user.name}</p>
+  <p>{user.email}</p>
+</li>
+)
   }
 }
